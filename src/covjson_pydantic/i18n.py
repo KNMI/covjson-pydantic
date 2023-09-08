3c02d03ef2956b175d103c7af2812f6407d0c3ec
Paul van Schayck <paul.van.schayck@knmi.nl>
2023-09-08 16:10:47 +0200

    KDP-1761: Ignored warning for i18n type

--- src/covjson_pydantic/i18n.py
+++ src/covjson_pydantic/i18n.py
@@ -9,4 +9,9 @@ class LanguageTag(str, Enum):
     undefined = "und"
 
 
-i18n = Dict[LanguageTag, str]
+# TODO: This was throwing warning:
+#   Expected `definition-ref` but got `LanguageTag` - serialized value may not be as expected
+# This may be a bug in Pydantic: https://github.com/pydantic/pydantic/issues/6467
+# So, for now, reverted to a less strict type
+# i18n = Dict[LanguageTag, str]
+i18n = Dict[str, str]
