8b911ffc9444959d2418569aa2105cd5074737f9
Paul van Schayck <paul.van.schayck@knmi.nl>
2023-09-22 11:46:45 +0200

    KDP-1761: Add note about issue

--- src/covjson_pydantic/i18n.py
+++ src/covjson_pydantic/i18n.py
@@ -14,5 +14,6 @@ class LanguageTag(str, Enum):
 # This may be a bug in Pydantic: https://github.com/pydantic/pydantic/issues/6467
 # or: https://github.com/pydantic/pydantic/issues/6422
 # So, for now, reverted to a less strict type
+# See issue: https://github.com/KNMI/covjson-pydantic/issues/3
 # i18n = Dict[LanguageTag, str]
 i18n = Dict[str, str]
